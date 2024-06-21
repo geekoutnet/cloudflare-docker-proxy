3d445967dc4387bd2e591583cf0f7d9093187350
ぃ慕容ぅ <969995851@qq.com>
2024-06-21 22:49:10 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.966788.xyz": dockerHub,
+  "quay.966788.xyz": "https://quay.io",
+  "gcr.966788.xyz": "https://gcr.io",
+  "k8s-gcr.966788.xyz": "https://k8s.gcr.io",
+  "k8s.966788.xyz": "https://registry.k8s.io",
+  "ghcr.966788.xyz": "https://ghcr.io",
+  "cloudsmith.966788.xyz": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.966788.xyz": dockerHub,
 };
 
 function routeByHosts(host) {
